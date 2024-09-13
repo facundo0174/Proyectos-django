352054dc31408f12029c1042948c066a4cb5ae78
facundo0174 <Facundo-agustin@outlook.es>
2024-09-13 17:53:37 -0300

    act en estructura

--- django-proyect-blogs/blog/manage.py
+++ django-proyect-blogs/blog/manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+    load_dotenv()
+    port=os.getenv('DJANGO_PORT')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +19,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if 'runserver' in sys.argv and port:
+        sys.argv.append(port)
     execute_from_command_line(sys.argv)
 
 
