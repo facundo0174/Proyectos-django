a0dbc7653f8dbe69d064434ab02f9ff1a75e33cc
facundo0174 <Facundo-agustin@outlook.es>
2024-09-18 23:06:09 -0300

    creacion de apps

--- django-proyect-blogs/blog/blog/urls.py
+++ django-proyect-blogs/blog/blog/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)'''
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    
+si coloco esto me dara error 404 generico pero de error en html, actualmente me da correctamente segun el mensaje
+generico de django como lo muestra el material complementario'''
