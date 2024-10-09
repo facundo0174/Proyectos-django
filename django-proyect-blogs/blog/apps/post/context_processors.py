24e84157b6ffa1142238efd518c3c12c7750c190
facundo0174 <Facundo-agustin@outlook.es>
2024-10-08 23:53:13 -0300

    contexto global para los NAVBAR

---
+++ django-proyect-blogs/blog/apps/post/context_processors.py
@@ -0,0 +1,7 @@
+from .models import Category
+
+def categories(request):
+    categories = list(Category.objects.all().order_by('category_name'))
+    categories.append({'category_name': 'Recientes', 'slug': 'Recientes'})
+
+    return {'categories': categories}
