9c8401a1903f6d216dfc278b18c259c5f41d06fb
facundo0174 <Facundo-agustin@outlook.es>
2024-10-15 21:39:07 -0300

    añadido manejo de errore falta templates para ello

--- django-proyect-blogs/blog/blog/views.py
+++ django-proyect-blogs/blog/blog/views.py
@@ -5,53 +5,15 @@ debera linkear/vincular un url para que pueda DEVOLVER la respuesta de forma vis
 #from django.http import HttpResponse
 #import datetime
 from django.views.generic import TemplateView
-# SIEMPRE con httpresponse se recibe como parametro una request o solicitud
-'''
-def saludo(request):
-    # tambien se puede definir una variable y llenarla de la cadena html correspondiente para su devolucion a django y esta lo devuelva 
-    '''
-'''documento = "<html> <head> <h1> Hola mundo </h1> </head> </html>" 
-        return HttpResponse(documento)'''
-
-'''
-    #esto es un muestreo estatico
-    return HttpResponse("hola mundo")# OJO PODEMOS ESCRIBIR EN HTML DENTRO DE LAS COMILLAS
-
-# muestro dinamico
-#muestra la fecha y hora actuales y se modifica en funcion de el momento de ejecucion
-
-def darfecha (request):
-    fechaActual=datetime.datetime.now()
-
-    documento='''
-''' <html>
-            <body>
-                <h1> Fecha actual es: %s
-                </h1>
-            </body>
-        </html>'''
-''' % fechaActual
-    
-    return HttpResponse(documento)
-
-def calcularanio(request,anio):
-    utilizando el url como obtencion de datos para realizar esta funcion, anio sera introducida en localHost/calcuarnio/2070
-siendo 2070 el año en el que se quiera saber que edad tendra la persona, django trabaja con los url de manera tal de que
-    todo lo que se escriba en la url del navegador lo toma como cadena de texto, por lo que o bien se lo formatea en la funcion en este
-    mismo modulo o en la llamada a la vista en urls.py
-    anio_act=2024
-    anio_futuro=anio
-    edad_actual=24
-    edad_futura= edad_actual + (anio_futuro - anio_act)
-    documento = 
-        <html>
-            <body>
-                <h1> La edad que tendria una persona de 24 años en el año %s seria de: %s
-                </h1>
-            </body>
-        </html> %(anio_act,edad_futura)
-    return HttpResponse(documento)'''
+from django.shortcuts import render
 
 class vistaindex(TemplateView):
         template_name='index.html'
         
+def not_found_view(request, exception):
+    return render(request, 'errors/error_not_found.html', status=404)
+def internal_error_view(request):
+    return render(request, 'errors/error_internal.html', status=500)
+def forbidden_view(request, exception):
+    return render(request, 'errors/error_forbidden.html', status=403)
+
