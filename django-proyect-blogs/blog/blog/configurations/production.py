d7aba0dc54ff95635a03ed9fc8ab331336a68e35
facundo0174 <Facundo-agustin@outlook.es>
2024-10-19 16:04:55 -0300

    prueba-deploit

--- django-proyect-blogs/blog/blog/configurations/production.py
+++ django-proyect-blogs/blog/blog/configurations/production.py
@@ -6,17 +6,12 @@ ALLOWED_HOST = ['127.0.0.1','localhost','dominio-produccion.com']
 
 DATABASES = {
     'default':{
-        'ENGINE':'django.db.backends.sqlite3',
-        'NAME':BASE_DIR / 'db_sqlite3',
-
-        #posgressql
-        # 'ENGINE':'django.db.backends.postgresql
-
-    #'NAME':os.getenv('DB_NAME'),
-    #'USER':os.getenv('DB_USER'),
-    #'PASSWORD':os.getenv('DB_PASSWORD'),
-    #'HOST':os.getenv('DB_HOST'),
-    #'PORT':os.getenv('DB_PORT'),
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':os.getenv('DB_NAME'),
+        'USER':os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST'),
+        'PORT':os.getenv('DB_PORT'),
     }
 }
 
